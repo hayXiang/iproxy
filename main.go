931e2de55cc93b075775075ba93dfc2415e842e3
hayXiang <86708160+hayXiang@users.noreply.github.com>
2024-01-22 23:03:36 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,33 +4,12 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 )
-
-var lOG_LEVEL = 3 //0: no log, 1: error, 2: info, 3: debug
-
-func LOG_DEBUG(out string) {
-	if lOG_LEVEL >= 3 {
-		log.Printf("[DEBUG]%s", out)
-	}
-}
-
-func LOG_INFO(out string) {
-	if lOG_LEVEL >= 2 {
-		log.Printf("[INFO]%s", out)
-	}
-}
-
-func LOG_ERROR(out string) {
-	if lOG_LEVEL >= 1 {
-		log.Printf("[ERROR]%s", out)
-	}
-}
 
 var transport = http.Transport{
 	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
@@ -42,9 +21,9 @@ var transport = http.Transport{
 type HttpRequestCallback func(*http.Request, *http.Client)
 
 func HttpGet(url string) (*http.Response, error) {
-	LOG_INFO("begin to HttpGet " + url)
+	fmt.Println("begin to HttpGet " + url)
 	defer func() {
-		LOG_INFO("end to HttpGet " + url)
+		fmt.Println("end to HttpGet " + url)
 	}()
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -66,14 +45,14 @@ func HttpGet(url string) (*http.Response, error) {
 }
 
 func proxy(w http.ResponseWriter, req *http.Request) {
-	LOG_INFO(req.RequestURI)
-	if !strings.Contains(req.RequestURI, ("http")) {
+	fmt.Println(req.RequestURI)
+	if !strings.Contains(req.RequestURI, ("/http:/")) && !strings.Contains(req.RequestURI, ("/https:/")) {
 		w.WriteHeader(500)
+		w.Write([]byte("must be start witch /http:/ or /https"))
 	} else {
 		url := req.RequestURI[1:]
 		url = strings.Replace(url, "http:/", "http://", 1)
 		url = strings.Replace(url, "https:/", "https://", 1)
-		fmt.Println(url)
 		resp, err := HttpGet(url)
 		if err != nil {
 			w.WriteHeader(500)
@@ -81,9 +60,14 @@ func proxy(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 		defer resp.Body.Close()
-		body, _ := io.ReadAll(resp.Body)
-		w.Write(body)
+		if resp.StatusCode == 301 || resp.StatusCode == 302 {
+			location := resp.Header.Get("Location")
+			location = strings.Replace(location, "http://", "/http:/", 1)
+			location = strings.Replace(location, "https://", "/https:/", 1)
+			w.Header().Set("Location", location)
+		}
 		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
 	}
 }
 func main() {
@@ -91,7 +75,6 @@ func main() {
 	if len(os.Args) > 1 {
 		listen_address = os.Args[1]
 	}
-
 	http.HandleFunc("/", proxy)
 	fmt.Println("server listen :" + listen_address)
 	http.ListenAndServe(listen_address, nil)
